f4e4a87342ee0fe413c0214f5515f5fbad4f7633
Pablo <percara.pablo@gmail.com>
2023-01-10 20:51:05 -0300

    aaah no cierra el programa

--- script.js
+++ script.js
@@ -13,29 +13,36 @@ let datos = [
 
 
 function inventario(datos){
-    let menu = prompt('Ingrese una opcion: \n1-Ingresar un nuevo producto\n2-Eliminar un producto existente\n3-Consultar stock\n4-Consultar ubicacion\n5-Listar base de datos\n6-SALIR');
-    while(menu!=6){
+    let opciones =[1,2,3,4,5]
+    let menu = prompt('Ingrese una opcion: \n1-Ingresar un nuevo producto\n2-Eliminar un producto existente\n3-Consultar stock\n4-Consultar ubicacion\n5-Listar base de datos\n\nIngrese cualquier otro valor para SALIR');
+    while(menu!=opciones){
         if(menu == 1){//-------------AGREGAR UN PRODUCTO A LA LISTA-----------------------------------
     //----------------------------------VALIDACIONES--------------------------------------------------
             let nuevoCodigo = prompt('Ingrese el codigo del producto: ');
+            if(nuevoCodigo == null){
+                inventario(datos)
+            }
             let validarCodigo = datos.some(dato => dato.codigo == nuevoCodigo);
             while(validarCodigo == true || nuevoCodigo == ''){
                 nuevoCodigo = prompt('El codigo ingresado es incorrecto o ya existe, ingrese otro: ')
                 validarCodigo = datos.some(dato => dato.codigo == nuevoCodigo);
             }
-    //------------------------------------------------------------------------------------------------------
+    
             let nuevoNombre = prompt('Ingrese el nombre del producto: ').toUpperCase();
             let validarNombre = datos.some(dato => dato.nombre == nuevoNombre);
             while(validarNombre == true || nuevoNombre ==''){
                 nuevoNombre = prompt('El nombre ingresado es incorrecto o ya existe, ingrese otro: ').toUpperCase();
                 validarNombre = datos.some(dato => dato.nombre == nuevoNombre);
             }
-    //--------------------------------------------------------------------------------------------------------
+   
             let stock = Number(prompt('Ingrese el stock: '))
+            if(stock == null){
+                inventario(datos)
+            }
             while(isNaN(stock) || stock == ''){
                 stock=Number(prompt('El valor ingresado no es numerico: '))
             }
-    //--------------------------------------------------------------------------------------------------------       
+         
             let nuevaUbicacion = prompt('Ingrese la ubicacion del producto: ').toUpperCase();
             let validarUbicacion = datos.some(dato => dato.ubicacion == nuevaUbicacion)
             while(validarUbicacion == true || nuevaUbicacion ==''){
@@ -58,6 +65,9 @@ function inventario(datos){
             }
         }else if(menu == 2){//------------ELIMINAR UN PRODUCTO---------------------------------------------
             let codigo = prompt("Ingrese el codigo o nombre del producto a eliminar: ")
+            if(codigo == null){
+                inventario(datos)
+            }
             let producto = datos.find(producto => (producto.codigo === codigo) || (producto.nombre === codigo.toUpperCase()))
             let busqueda = datos.findIndex( producto => (producto.codigo === codigo) || (producto.nombre === codigo.toUpperCase()))
             let respuesta = confirm(`Seguro desea eliminar el producto ${producto.nombre}, en el índice ${busqueda}?`)
@@ -65,19 +75,25 @@ function inventario(datos){
                     datos.splice(busqueda, 1)
                     alert("Producto eliminado!")
                     inventario(datos)
-                };
-            inventario(datos);
+                }else{inventario(datos)}
         }else if(menu == 3){//-----------CONSULTAR STOCK-----------------------------------------------------------
             let codigo = prompt("Ingrese el codigo o nombre del producto buscado") 
+            if(codigo == null){
+                inventario(datos)
+            }
             let busqueda = datos.find( producto => (producto.codigo === codigo) || (producto.nombre === codigo.toUpperCase()))
             let resultado = confirm(`El stock disponible del producto ${busqueda.nombre} es de ${busqueda.stock} unidades\n Desea realizar otra busqueda?`)
-            if(!resultado){
+            if(!resultado || resultado){
                 inventario(datos);
             }
         }else if(menu == 4){//----------CONSULTAR UBICACION---------------------------------------------------------
             let codigo = prompt("Consultar Ubicación por codigo o nombre") 
+            if(codigo==null){
+                inventario(datos)
+            }
             let busqueda = datos.find( producto => (producto.codigo === codigo) || (producto.nombre === codigo.toUpperCase()))
             alert(`La ubicación del producto ${busqueda.nombre} es ${busqueda.ubicacion}`);
+            inventario(datos);
 
         }else if( menu == 5){//-----------LISTAR BASE DE DATOS--------------------------------------------------------
             let vista = 'Productos disponibles en la base de datos: \n\n';
@@ -85,10 +101,20 @@ function inventario(datos){
             alert(vista);
             inventario(datos);
         }else{
-            alert('La opcion ingresada es incorrecta')
-            inventario(datos)
+            let consulta = confirm('Desea terminar el programa?')
+            if(!consulta){
+                inventario(datos)
+            }else{
+                
+            }
+
+
+            
         }
+
+        
     }
+   
     
 }
 
